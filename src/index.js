f7021f16f1aa88e26cff16fccc26e6b98e467177
jistjoalal <jistjoalal@gmail.com>
2019-04-20 16:33:41 -0700

    basic graphql server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import { GraphQLServer } from 'graphql-yoga';
+
+// type defs
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+// resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'First query';
+    },
+    name() {
+      return 'Andrew Mead';
+    },
+    location() {
+      return 'The moon';
+    },
+    bio() {
+      return "I'm the guy on the moon";
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+server.start(() => {
+  console.log('server running')
+})
